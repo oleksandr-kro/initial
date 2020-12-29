c6dd6d63f5af2db062068b3fd293c04a4f1e4d14
TarasChuchvara <T.Chuchvara@ukr.net>
2020-12-29 18:59:20 +0200

    Add project

---
+++ Java/EveryMatrixShipsProject/src/Main.java
@@ -0,0 +1,40 @@
+
+public class Main {
+
+    static int[][] ships = {
+            {0, 0, 0, 0, 0, 0, 0, 1, 0, 0,},
+            {0, 1, 0, 0, 0, 0, 0, 1, 0, 0,},
+            {0, 1, 0, 1, 1, 0, 0, 0, 0, 0,},
+            {0, 1, 0, 1, 1, 0, 0, 1, 1, 1,},
+            {0, 1, 0, 0, 0, 0, 0, 0, 0, 0,},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0,},
+            {1, 1, 1, 1, 0, 1, 0, 0, 0, 0,},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0,},
+            {1, 1, 0, 0, 0, 0, 0, 1, 0, 0,},
+            {1, 1, 0, 0, 0, 0, 0, 0, 0, 1,}
+    };
+
+    public static void main(String[] args) {
+        int count = 0;
+
+
+        for (int line = 0; line < ships.length; line++) {
+            for (int pos = 0; pos < ships[line].length; pos++) {
+
+                if (ships[line][pos] == 1) {
+                    int tempLine = line;
+                    int tempPos = pos;
+                    if (++tempPos < ships[line].length && ships[line][tempPos] == 1) {
+                        continue;
+                    } else if (++tempLine < ships.length && ships[tempLine][pos] == 1) {
+                        continue;
+                    } else {
+                        count++;
+                    }
+                }
+            }
+        }
+
+        System.out.println(count);
+    }
+}
